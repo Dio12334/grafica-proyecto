0ead64c8d4f699edb67f7e7f9b1545867de637e0
jeanpaulHT <jean.huby@utec.edu.pe>
2022-09-13 15:05:35 -0500

    extra files fixe

--- Light.cpp
+++
@@ -1,6 +0,0 @@
-#include "Light.h"
-#include "World.h"
-
-Light::Light(Math::Vector<float> pos, Color col): position(pos), color(col){
-	
-}
--- Ray.cpp
+++
@@ -1,5 +0,0 @@
-#include "Ray.h"
-
-Ray::Ray(Point<float> o, Vector<float> dir): origin(o), direction(dir){
-
-}
